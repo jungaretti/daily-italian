bcb3f945b7e095d976779b61072cba16a23e5dde
JP Ungaretti <git@jungaretti.com>
2023-10-15 13:17:59 -0700

    Add new framework

---
+++ DailyLibrary/DailyLibrary.h
@@ -0,0 +1,18 @@
+//
+//  DailyLibrary.h
+//  DailyLibrary
+//
+//  Created by JP Ungaretti on 10/15/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DailyLibrary.
+FOUNDATION_EXPORT double DailyLibraryVersionNumber;
+
+//! Project version string for DailyLibrary.
+FOUNDATION_EXPORT const unsigned char DailyLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DailyLibrary/PublicHeader.h>
+
+
