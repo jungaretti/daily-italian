6aab0158d708346159d16b8692f5fcf342d8c0ea
JP Ungaretti <git@jungaretti.com>
2023-10-15 11:18:49 -0700

    Add widget extension

---
+++ DailyWidget/DailyWidget.swift
@@ -0,0 +1,82 @@
+//
+//  DailyWidget.swift
+//  DailyWidget
+//
+//  Created by JP Ungaretti on 10/15/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), emoji: "😀")
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date(), emoji: "😀")
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate, emoji: "😀")
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let emoji: String
+}
+
+struct DailyWidgetEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Time:")
+                Text(entry.date, style: .time)
+            }
+
+            Text("Emoji:")
+            Text(entry.emoji)
+        }
+    }
+}
+
+struct DailyWidget: Widget {
+    let kind: String = "DailyWidget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            if #available(macOS 14.0, iOS 17.0, *) {
+                DailyWidgetEntryView(entry: entry)
+                    .containerBackground(.fill.tertiary, for: .widget)
+            } else {
+                DailyWidgetEntryView(entry: entry)
+                    .padding()
+                    .background()
+            }
+        }
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
+
+#Preview(as: .systemSmall) {
+    DailyWidget()
+} timeline: {
+    SimpleEntry(date: .now, emoji: "😀")
+    SimpleEntry(date: .now, emoji: "🤩")
+}
---
+++ DailyWidget/DailyWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DailyWidgetBundle.swift
+//  DailyWidget
+//
+//  Created by JP Ungaretti on 10/15/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DailyWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DailyWidget()
+    }
+}
