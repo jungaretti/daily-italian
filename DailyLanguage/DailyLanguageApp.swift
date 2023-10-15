718118ab4e7db3ca54573014d8dd369c3efc6800
JP Ungaretti <git@jungaretti.com>
2023-10-15 11:12:32 -0700

    First commit

---
+++ DailyLanguage/DailyLanguageApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyLanguageApp.swift
+//  DailyLanguage
+//
+//  Created by JP Ungaretti on 10/15/23.
+//
+
+import SwiftUI
+
+@main
+struct DailyLanguageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
