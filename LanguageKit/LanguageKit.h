589a3005be5ab7ad6ac90d971f1ceff7567aa164
JP Ungaretti <git@jungaretti.com>
2023-11-07 17:07:33 -0800

    Create new framework

---
+++ LanguageKit/LanguageKit.h
@@ -0,0 +1,18 @@
+//
+//  LanguageKit.h
+//  LanguageKit
+//
+//  Created by JP Ungaretti on 11/7/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LanguageKit.
+FOUNDATION_EXPORT double LanguageKitVersionNumber;
+
+//! Project version string for LanguageKit.
+FOUNDATION_EXPORT const unsigned char LanguageKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LanguageKit/PublicHeader.h>
+
+
