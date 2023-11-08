6395bd5dac26966ad1a54f457b8a4401c69ec10a
JP Ungaretti <git@jungaretti.com>
2023-11-08 07:19:58 -0800

    Add simple unit test for LanguageKit

--- LanguageKitTests/LanguageKitTests.swift
+++ LanguageKitTests/LanguageKitTests.swift
@@ -9,28 +9,14 @@ import XCTest
 @testable import LanguageKit
 
 class LanguageKitTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testTranslationProviderRandom() {
+        let translation = Translation(from: (.English, "love"), to: (.Italian, "amore"))
+        let translationProvider = TranslationProvider(translations: [translation])
+        
+        let randomTranslation = translationProvider.randomTranslations(from: .English, to: .Italian)
+        XCTAssert(randomTranslation?.from.language == .English)
+        XCTAssert(randomTranslation?.from.text == "love")
+        XCTAssert(randomTranslation?.to.language == .Italian)
+        XCTAssert(randomTranslation?.to.text == "amore")
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
-}   
+}
