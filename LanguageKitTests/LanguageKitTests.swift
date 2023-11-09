cbc8b886a2b6ba042120fe97ed294110728adbbd
JP Ungaretti <git@jungaretti.com>
2023-11-08 22:21:59 -0800

    Add test for Italian translation

--- LanguageKitTests/LanguageKitTests.swift
+++ LanguageKitTests/LanguageKitTests.swift
@@ -9,7 +9,7 @@ import XCTest
 @testable import LanguageKit
 
 class LanguageKitTests: XCTestCase {
-    func testTranslationProviderRandom() {
+    func testTranslationProviderRandomSingle() {
         let translation = Translation(from: (.English, "love"), to: (.Italian, "amore"))
         let translationProvider = TranslationProvider(translations: [translation])
         
@@ -18,5 +18,13 @@ class LanguageKitTests: XCTestCase {
         XCTAssert(randomTranslation?.from.text == "love")
         XCTAssert(randomTranslation?.to.language == .Italian)
         XCTAssert(randomTranslation?.to.text == "amore")
+    }
+
+    func testTranslationProviderRandomItalian() {
+        let translationProvider = globalProvider
+
+        let randomTranslation = translationProvider.randomTranslation(from: .English, to: .Italian)
+        XCTAssert(randomTranslation?.from.language == .English)
+        XCTAssert(randomTranslation?.to.language == .Italian)
     }
 }
