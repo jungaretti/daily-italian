958e7b30db9d01b9b1f29a9c3016cf226c790c40
JP Ungaretti <git@jungaretti.com>
2023-11-08 21:46:06 -0800

    Use typealias instead of struct

--- LanguageKit/Translation.swift
+++ LanguageKit/Translation.swift
@@ -7,12 +7,4 @@
 
 import Foundation
 
-public struct Translation {
-    public let from: Word
-    public let to: Word
-    
-    public init(from: Word, to: Word) {
-        self.from = from
-        self.to = to
-    }
-}
+public typealias Translation = (from: Word, to: Word)
