16570b7220950f210c7b8f3bb0cab13b8d0da147
JP Ungaretti <git@jungaretti.com>
2024-03-09 14:12:40 -0800

    Create TranslationView

--- DailyWidget/DailyWidget.swift
+++ DailyWidget/DailyWidget.swift
@@ -9,36 +9,6 @@ import WidgetKit
 import SwiftUI
 import DailyLibrary
 
-struct TranslationView : View {
-    var entry: TranslationTimelineProvider.Entry
-
-    var body: some View {
-        VStack {
-            HStack {
-                Text(entry.translation.to.text)
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .lineLimit(1)
-                    .minimumScaleFactor(0.2)
-                Spacer()
-            }
-            HStack {
-                Text(entry.translation.from.text)
-                    .font(.system(.body))
-                Spacer()
-            }
-
-            Spacer()
-
-            HStack {
-                Text(String(entry.translation.to.language.flagEmoji))
-                    .font(.title)
-                Spacer()
-            }
-        }
-    }
-}
-
 struct TranslationWidget: Widget {
     let kind: String = "DailyWidget"
 
---
+++ DailyWidget/TranslationView.swift
@@ -0,0 +1,39 @@
+//
+//  TranslationView.swift
+//  DailyWidgetExtension
+//
+//  Created by JP Ungaretti on 3/9/24.
+//
+
+import SwiftUI
+import DailyLibrary
+
+struct TranslationView : View {
+    var entry: TranslationTimelineProvider.Entry
+
+    var body: some View {
+        VStack {
+            HStack {
+                Text(entry.translation.to.text)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.2)
+                Spacer()
+            }
+            HStack {
+                Text(entry.translation.from.text)
+                    .font(.system(.body))
+                Spacer()
+            }
+
+            Spacer()
+
+            HStack {
+                Text(String(entry.translation.to.language.flagEmoji))
+                    .font(.title)
+                Spacer()
+            }
+        }
+    }
+}
